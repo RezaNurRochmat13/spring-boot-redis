3f7acd28cc687453e9f185a3250c5188bc88903b
Reja <rezanurrochmat@gmail.com>
2022-03-23 16:33:20 +0700

    Add jedis connection

---
+++ src/main/java/com/spring/boot/redis/configuration/RedisConfiguration.java
@@ -0,0 +1,33 @@
+package com.spring.boot.redis.configuration;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+
+@Configuration
+public class RedisConfiguration {
+    @Value("${spring.datasource.redis.host}")
+    private String redisHost;
+
+    @Value("${spring.datasource.redis.port}")
+    private Integer redisPort;
+
+    @Bean
+    JedisConnectionFactory jedisConnectionFactory() {
+        JedisConnectionFactory jedisConnectionFactory = new JedisConnectionFactory();
+        jedisConnectionFactory.setHostName(redisHost);
+        jedisConnectionFactory.setPort(redisPort);
+
+        return jedisConnectionFactory;
+    }
+
+    @Bean
+    public RedisTemplate<String, Object> redisTemplate() {
+        RedisTemplate<String, Object> redisTemplate = new RedisTemplate<>();
+        redisTemplate.setConnectionFactory(jedisConnectionFactory());
+
+        return redisTemplate;
+    }
+}
