5c81099f4f5bdaddb37a07b86a09701997a8a2ec
Reja <rezanurrochmat@gmail.com>
2022-03-25 14:33:44 +0700

    Add swagger docs

---
+++ src/main/java/com/spring/boot/redis/configuration/SwaggerConfiguration.java
@@ -0,0 +1,40 @@
+package com.spring.boot.redis.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.service.VendorExtension;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+
+import java.util.ArrayList;
+
+@Configuration
+public class SwaggerConfiguration {
+    @Bean
+    public Docket dockerApi() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.any())
+                .paths(PathSelectors.any())
+                .build()
+                .apiInfo(metaInfo());
+    }
+
+    private ApiInfo metaInfo() {
+
+        return new ApiInfo(
+                "Spring Boot Redis API REST",
+                "Redis API",
+                "1.0",
+                "Terms of Service",
+                new Contact("Reja", "github.com/RezaNurRochmat13",
+                        " "),
+                "Apache License Version 2.0",
+                "https://www.apache.org/licesen.html", new ArrayList<VendorExtension>()
+        );
+    }
+}
--- src/main/java/com/spring/boot/redis/configuration/WebSecurityConfiguration.java
+++ src/main/java/com/spring/boot/redis/configuration/WebSecurityConfiguration.java
@@ -8,12 +8,28 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
+    private static final String[] AUTH_WHITELIST = {
+            // -- Swagger UI v2
+            "/v2/api-docs",
+            "/swagger-resources",
+            "/swagger-resources/**",
+            "/configuration/ui",
+            "/configuration/security",
+            "/swagger-ui.html",
+            "/webjars/**",
+            // -- Swagger UI v3 (OpenAPI)
+            "/v3/api-docs/**",
+            "/swagger-ui/**"
+    };
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .csrf().disable()
                 .authorizeRequests()
                 .antMatchers("/api/v1/**").permitAll()
+                .antMatchers("/swagger-ui/**").permitAll()
+                .antMatchers(AUTH_WHITELIST).permitAll()
                 .anyRequest()
                 .authenticated();
     }
