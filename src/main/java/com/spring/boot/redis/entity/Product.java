b17feb970678e2343e941421fbe737d8bfb32090
Reja <rezanurrochmat@gmail.com>
2022-10-19 13:16:04 +0700

    add entity product

---
+++ src/main/java/com/spring/boot/redis/entity/Product.java
@@ -0,0 +1,21 @@
+package com.spring.boot.redis.entity;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.data.redis.core.RedisHash;
+
+import javax.persistence.Id;
+import java.io.Serializable;
+
+@RedisHash(value = "Product", timeToLive = 1500)
+@Getter
+@Setter
+@NoArgsConstructor
+public class Product implements Serializable {
+    @Id
+    private Long id;
+    private String name;
+    private String description;
+    private String qty;
+}
